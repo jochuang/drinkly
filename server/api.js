b5d9260bb4ebc7d19b15d6826551da228c625fa6
Jo Huang <johuang@Jos-MacBook-Air.local>
2022-11-23 15:10:29 -0500

    adding server.js and api.js

--- server/api.js
+++ server/api.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const controller = require('./controller');
 const router = express.Router();
-// ​
-// router.get('/',
-//   controller.getDrinksByName,
-//   (req, res) => {
-//     res.status(200).json(res.locals.cocktails)
-//   }
-// );
-// ​
+
+router.get('/', (req,res) => {
+    res.status(200).json('hi');
+});
+
 module.exports = router;
