5b98fda9920cb0bdd105e16cab86c5bf3b32b9f2
Jo Huang <joh0324@gmail.com>
2022-12-24 01:14:36 -0500

    node-fetch works on the backend. axios does not

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,20 @@
+// const axios = require('axios');
+const fetch = require('node-fetch')
+ 
 const controller = {
     getDrinksByName: (req, res, next) => {
-        res.locals.test = 'in the controller'
-        return next()
+
+        const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=mojito'
+
+        fetch(url)
+            .then(resp => resp.json())
+            .then(resp => {
+                console.log('in getDrinksByName Controller')
+                console.log(resp)
+                res.locals.drinks = resp.drinks;
+                return next()
+            })
+            // .catch((e) => console.log(e))
     }
 }
 
