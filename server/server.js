f93260dd80e1e98f482b0d271dea0566d00fcf22
Jo Huang <joh0324@gmail.com>
2022-12-24 01:14:36 -0500

    adding server.js and api.js

--- server/server.js
+++ server/server.js
@@ -1,19 +1,14 @@
-const path = require('path');
-const express = require ('express');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
 // require router
 const apiRouter = require('./api');
 
-// handle parsing request body
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// serving static component
-app.use(express.static(path.resolve(__dirname, '../client')));
 // define route handler
-// app.use('/api', apiRouter);
+app.use('/api', apiRouter);
 
+// start server
 app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}...`);
+    console.log(`Server listening on port: ${PORT}`);
 });
