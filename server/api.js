b53c63eb8f445b3cb16f37a33cd20c17e84266b2
Jo Huang <joh0324@gmail.com>
2022-12-24 01:14:36 -0500

    working on server

---
+++ server/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// const controller = require('./controller');
+const router = express.Router();
+// ​
+// router.get('/',
+//   controller.getDrinksByName,
+//   (req, res) => {
+//     res.status(200).json(res.locals.cocktails)
+//   }
+// );
+// ​
+module.exports = router;
